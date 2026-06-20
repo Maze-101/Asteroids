a83bc028593717afa7f35bafe2864a034ac6de7a
shikigami <mazenkassem101@gmail.com>
2026-06-20 08:32:47 +0300

    feat: add the player to the game

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+# Screen
+SCREEN_WIDTH : int = 1280
+SCREEN_HEIGHT : int = 720
+
+# Player
+PLAYER_RADIUS : int = 20
+LINE_WIDTH : int = 2
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
-from pygame import * # type: ignore
+from pygame import *
 from pygame.time import Clock
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import *
 from logger import log_state
 
 def main():
@@ -12,12 +13,14 @@ def main():
     screen : Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock : Clock = pygame.time.Clock()
     dt : float = 0.0
+    player : Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         display.flip()
         dt = clock.tick(60) / 1000
 
